9288392defd07e21bd65250cebdaaa64b3d98f1f
cnazemi <cameronsnazemi@gmail.com>
2021-05-15 19:17:48 -0700

    Added service worker event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,17 @@ window.onpopstate = function(event) {
   setState({'page': "entry"}, true)
   // alert(`location: ${document.location}, state: ${JSON.stringify(event.state)}`)
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
+
+    // navigator.serviceWorker.register('../sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
